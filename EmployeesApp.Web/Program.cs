e91ba947c9d1e412850dd56b1c02bbb72107b66f
Jörgen Hägglund <jorgen@sirlucifer.mine.nu>
2025-05-19 14:15:22 +0200

    Fixade uppgift 1.a.a, 1.a.b och 1.a.c, vilka vi glömde först.

--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -1,9 +1,17 @@
+using System.Globalization;
+
 namespace EmployeesApp.Web
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            CultureInfo cultureInfo = CultureInfo.CreateSpecificCulture("sv-SE");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
             var app = builder.Build();
